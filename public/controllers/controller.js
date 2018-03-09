1a2291e709c7504450a4ffe4b1d802b7a4a0ae9a
fereja <fereja@gmail.com>
2018-03-08 22:51:02 -0800

    dami data add

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -8,6 +8,29 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', 
 function($scope, $http) {
     console.log("Hello World from controller");
+    
+    $http.get('/contactlist').then(function(response){
+    console.log("I got the data I requested");
+    
+    $scope.contactlist = response;
+    })
+  /*  person1 = {
+        name :'jone',
+        email:'jone@email.com',
+        phone:'206-343-4343'
+    };
+    person2={
+        name:'Ema',
+        email:'ema@mail.com',
+        phone:'202-343-8977'
+    };
+    person3={
+        name:'samri',
+        email:'samri@aol.com',
+        phone:'204-456-8932'
+    }
+    var contactlist =[person1,person2,person3];
+    $scope.contactlist=contactlist;*/
 }]);
 
 
--- server.js
+++ server.js
@@ -7,5 +7,26 @@ var app  = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactlist',function(req, res){
+console.log("I recive get Request");
+person1 = {
+    name :'jone',
+    email:'jone@email.com',
+    phone:'206-343-4343'
+};
+person2={
+    name:'Ema',
+    email:'ema@mail.com',
+    phone:'202-343-8977'
+};
+person3={
+    name:'samri',
+    email:'samri@aol.com',
+    phone:'204-456-8932'
+};
+var contactlist =[person1,person2,person3];
+res.json(contactlist);
+});
+
 app.listen(3000);
 console.log("server running on port 3000");
